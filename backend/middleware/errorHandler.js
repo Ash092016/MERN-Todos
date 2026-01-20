eb0f33410db8338bb4ec5af62c64be3c6bef2261
Ashutosh <ashutoh182005@gmail.com>
2026-01-20 17:20:05 +0530

    ErrorHandler middleware

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,33 @@
+//Error Handler middleware which catches and formats the error from all routes 
+
+const errorHandler=(err,req,res,next)=>{
+    let statusCode=res.statusCode===200?500:res.statusCode
+    let message=err.message
+
+    if(err.name==='ValidationError'){
+        statusCode=400
+        message=Object.values(err.errors).map(val => val.message).join(', ')
+    }
+
+    if(err.name==='CastError' && err.kind==='ObjectId'){
+        statusCode=404
+        message='Resource not found'
+    }
+
+    if(err.name === 'JsonWebTokenError') {
+        statusCode=401
+        message='Invalid token, please login again'
+    }
+
+    if(err.name === 'TokenExpiredError') {
+        statusCode=401
+        message='Token expired, please login again'
+    }
+
+    res.status(statusCode).json({
+        success:false,
+        message,
+        stack:process.env.NODE_ENV==='production'?null:err.stack
+    })
+}
+module.exports={errorHandler}
