862747c7bc5460e0475d7b08c78a5ac063127f0c
Ashutosh <ashutoh182005@gmail.com>
2026-01-20 22:53:18 +0530

    Server.js file

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const dotenv=require('dotenv')
+const cors=require('cors')
+const connectDB=require('./config/db')
+const {errorHandler}=require('./middleware/errorHandler')
+
+const authRoutes=require('./routes/auth')
+const todoRoutes=require('./routes/todo')
+
+//Loading environment variables
+dotenv.config()
+
+//Connect to MongoDB
+connectDB()
+
+//Initializing express app
+const app=express()
+
+//Global Middleware
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.use('/api/auth',authRoutes)
+app.use('/api/todos',todoRoutes)
+
+app.get('/',(req, res)=>{
+  res.json({message:'MERN Todo API is running'})
+})
+
+//Error Handling Middleware
+app.use(errorHandler)
+
+const PORT=process.env.PORT||5000
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
