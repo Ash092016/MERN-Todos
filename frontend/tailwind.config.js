1f4a46f8cac0c5a7fdd635b1dc1e01e07ff601d1
Ashutosh <ashutoh182005@gmail.com>
2026-01-24 16:28:52 +0530

    Tailwind config updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       animation:{
-        'pulse-slow':'pulse 3s cubic bezier(0.4,0,0.6,1) infinite',
+        'pulse-slow':'pulse 3s cubic-bezier(0.4,0,0.6,1) infinite',
       }
     },
   },
