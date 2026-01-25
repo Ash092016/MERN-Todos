a3989fdf263691a4936bf649d38c3124f01e168a
Ashutosh <ashutoh182005@gmail.com>
2026-01-25 16:46:43 +0530

    feat: create User model with schema definition, password hashing, and comparison method.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,50 +1,49 @@
-const mongoose=require('mongoose')
-const bcrypt=require('bcryptjs')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
-const Schema=mongoose.Schema
+const Schema = mongoose.Schema
 
 //Defining the User Schema 
-const UserSchema=new Schema(
+const UserSchema = new Schema(
     {
-        name:{
-            type:String,
-            required:[true,'Please add a name'],
-            trim:true,
+        name: {
+            type: String,
+            required: [true, 'Please add a name'],
+            trim: true,
         },
-        email:{
-            type:String,
-            required:[true,'Please add an email'],
-            unique:true,
-            lowercase:true,
-            trim:true,
-            match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        email: {
+            type: String,
+            required: [true, 'Please add an email'],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                 'Please add a valid email address',],
         },
-        password:{
-            type:String,
-            required:[true,'Please add a password'],
-            minlength:6,
-            select:false,
+        password: {
+            type: String,
+            required: [true, 'Please add a password'],
+            minlength: 6,
+            select: false,
         },
     },
     {
-        timestamps:true,
+        timestamps: true,
     }
 )
 
 //Hashing the password before saving new user into the database
-UserSchema.pre('save',async function(next){
-    if(!this.isModified('password')){
-        return next()
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) {
+        return;
     }
 
-    const salt=await bcrypt.genSalt(10)
-    this.password=await bcrypt.hash(this.password,salt)
-    next();
-})
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
-UserSchema.methods.matchPassword=async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword,this.password)
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
 }
 
-module.exports=mongoose.model('User',UserSchema)
+module.exports = mongoose.model('User', UserSchema)
